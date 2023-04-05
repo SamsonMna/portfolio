08f04ae2143ac5ed08729fbe30915a943b325e8c
SamsonMna <116483061+SamsonMna@users.noreply.github.com>
2023-04-05 08:35:39 +0300

    Create internet download.py
    Download Stuff from the internet using python's Internet Download manager module

---
+++ internet download.py
@@ -0,0 +1,10 @@
+# Lets download something on the internet
+import internetdownloadmanager as idm
+
+def Loader(url, download):
+    pyloader = idm.Downloader(worker=20,
+        part_size=10000,
+        resumable=True)
+    pyloader.download(url, output)
+Loader('input url link to your download wish', 'input the expected output')
+Loader('youtube.com', 'video.mkv')
