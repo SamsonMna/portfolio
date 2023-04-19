9be2c63fced9d787a65c49cc73637425a3c2a591
Samson  Mn <116483061+SamsonMna@users.noreply.github.com>
2023-04-19 19:36:53 +0300

    Update api requests.py
    API request response in JSON format.

--- api requests.py
+++ api requests.py
@@ -5,7 +5,7 @@ import json
 url = 'abc.com/dce/xyz'
 https = urllib.PoolManager()
 response = https.request('GET', url)
-print(response.data) # the xyz of the dce folder in your url.
+print(response.data) # the xyz of the dce folder in your url. Has to be in a key-value format else we use the response.content in bs4
 # Make a post to api to receive data in form of a Json or csv or yaml
 https = urllib.PoolManager()
 response = https.request('POST', url, fields={'whichever fields you want return in a a dictionary format: "abc" : "xyz"'})
